52b3c1737d5d1273a5eea6c929e6afd767b8d9b3
sameplacewei <sameplace@126.com>
2023-12-08 10:51:23 +0800

    evm upgrade

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -449,11 +449,13 @@ func (evm *EVM) create(caller ContractRef, codeAndHash *codeAndHash, gas uint64,
 	if !evm.Context.CanTransfer(evm.StateDB, caller.Address(), value) {
 		return nil, common.Address{}, gas, ErrInsufficientBalance
 	}
-	// nonce := evm.StateDB.GetNonce(caller.Address())
-	// if nonce+1 < nonce {
-	// 	return nil, common.Address{}, gas, ErrNonceUintOverflow
-	// }
-	// evm.StateDB.SetNonce(caller.Address(), nonce+1)
+
+	nonce := evm.StateDB.GetNonce(caller.Address())
+	if nonce+1 < nonce {
+		return nil, common.Address{}, gas, ErrNonceUintOverflow
+	}
+	evm.StateDB.SetNonce(caller.Address(), nonce+1)
+
 	// We add this to the access list _before_ taking a snapshot. Even if the creation fails,
 	// the access-list change should not be rolled back
 	if evm.chainRules.IsBerlin {
